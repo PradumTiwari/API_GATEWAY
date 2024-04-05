daf73a2fd274628ea7b08ae1c26ac78da66fd607
Pradum <satendarpradum@gmail.com>
2024-04-06 00:33:52 +0530

    Done By Making A Api Gateway

---
+++ README.md
@@ -0,0 +1,10 @@
+Actual Goal of Readme.md
+
+
+Apart From frontend and backend People Also Talk About MIDDLE-END
+
+-We need An Intermediate Layer between the client side and The micorservices
+
+-USing the Middle End,when the client sends request we will be able to make Decision that which microservice should Actually respond to this request
+
+-we can do message validation.response transformation,rate limmiting,
---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require('express');
+const morgan=require('morgan');
+const axios=require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const rateLimit = require("express-rate-limit");
+const app=express();
+
+const PORT=3005;
+
+const limiter=rateLimit({
+    windows:2*60*1000,
+    max:500,
+})
+
+
+app.use(morgan('combined'));
+app.use(limiter);
+
+app.use('/bookingservice',async(req,res,next)=>{
+    console.log(req.headers['x-access-token']);
+
+   try {
+    const response=await axios.get('http://localhost:3000/api/v1/isAuthenticated',{
+        headers:{
+            'x-access-token':req.headers['x-access-token']
+        }
+    });
+    console.log(response.data);
+    console.log('Booking Service');
+    if(response.data.sucess){
+    next();
+}
+    else{
+        res.status(401).send('Unauthorized');
+    }
+   } catch (error) {
+return res.status(401).json({
+    error:'Unauthorized'
+})    
+   }
+})
+
+app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3002',changeOrigin:true}));
+app.use('/ReminderService',createProxyMiddleware({target:'http://localhost:3004',changeOrigin:true}));
+app.get('/home',(req,res)=>{
+    res.send('Hello Pradum');
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
